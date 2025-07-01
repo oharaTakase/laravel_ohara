ac831bffea37badc9adaffd42033453f2a107140
Takase <M2420038@ngo.ohara.ac.jp>
2025-07-01 12:30:22 +0900

    0701 index, create

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,26 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Support\Facades\Route;
 
 // routing for test
 Route::get('tests/test', [TestController::class, 'index'])->name('test.index');
+
+// お問い合わせフォーム用ルート
+// Route::get('/cantacts',[ContactFormContoller::class, 'index'])->name('contacts.index');
+
+// リソースコントローラー用のまとめてルート作成
+// Route::resource('contacts', ContactFormController::class);
+
+Route::prefix('contacts') // 頭に contacts をつける 
+->middleware(['auth']) // 認証 
+->name('contacts.') // ルート名 
+->controller(ContactFormController::class) // コントローラ指定 
+->group(function(){ // グループ化 
+	Route::get('/', 'index')->name('index'); // 名前つきルート 
+	Route::get('/create', 'create')->name('create'); 
+}); 
 
 
 Route::get('/', function () {
